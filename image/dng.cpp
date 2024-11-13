22ad0f0109c2e310e3771184d318242aa24a041d
Matthew Green <mgreen@wassoc.com>
2024-11-13 11:51:30 -0800

    left 1

--- image/dng.cpp
+++ image/dng.cpp
@@ -503,14 +503,14 @@ void dng_save(void *mem, StreamInfo const &info, ControlList const &metadata,
 		std::cout << "mem1: " << (uint8_t*)mem + 1 << std::endl;
 		std::cout << "mem2: " << (uint8_t*)mem + 2 << std::endl;
 		std::cout << "mem0: " << mem << std::endl;
-		uint8_t *toPrint = (uint8_t*)mem;
+		uint8_t *toPrint = (uint8_t*)mem - 1;
 		for(unsigned int i = 0; i < 30; i++) {
 			std::cout << i << ":\t" << (int)toPrint[i] << "\t" << std::bitset<8>(toPrint[i]) << std::endl;
 		}
 
 		for (unsigned int y = 0; y < info.height; y++)
 		{
-			void *writeLoc = (void*)((uint8_t*)mem + (info.stride * y));
+			void *writeLoc = (void*)((uint8_t*)mem + (info.stride * y) - 1);
 			// std::cout << "loc: " << writeLoc << std::endl;
 			if (TIFFWriteScanline(tif, writeLoc, y, 0) != 1)
 				throw std::runtime_error("error writing DNG image data");
