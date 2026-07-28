4630f6e01d8c09d39d11b96bfe8862948d2bb597
Matthew Green <mgreen@wassoc.com>
2026-07-28 16:05:27 -0700

    try and speed up auto exposure frames

--- apps/rpicam_raw.cpp
+++ apps/rpicam_raw.cpp
@@ -36,17 +36,23 @@ bool isAutoExposureEnabled(VideoOptions const *options)
 
 static void enableAutoExposure(RPiCamApp &app)
 {
+	constexpr int64_t frame_time = 1000000 / 20; // 50,000 us = 20 fps
 	libcamera::ControlList cl;
 	cl.set(libcamera::controls::ExposureTimeMode, libcamera::controls::ExposureTimeModeAuto);
 	cl.set(libcamera::controls::AnalogueGainMode, libcamera::controls::AnalogueGainModeAuto);
+	cl.set(libcamera::controls::FrameDurationLimits,
+		   libcamera::Span<const int64_t, 2>({ frame_time, frame_time }));
 	app.SetControls(cl);
 }
 
 static void lockAutoExposure(RPiCamApp &app)
 {
+	int64_t frame_time = 1000000 / app.GetOptions()->Get().framerate.value_or(DEFAULT_FRAMERATE); // in us
 	libcamera::ControlList cl;
 	cl.set(libcamera::controls::ExposureTimeMode, libcamera::controls::ExposureTimeModeManual);
 	cl.set(libcamera::controls::AnalogueGainMode, libcamera::controls::AnalogueGainModeManual);
+	cl.set(libcamera::controls::FrameDurationLimits,
+		   libcamera::Span<const int64_t, 2>({ frame_time, frame_time }));
 	app.SetControls(cl);
 }
 
@@ -218,7 +224,7 @@ protected:
 
 static void event_loop(LibcameraRaw &app, GpioHandler* lampHandler)
 {
-	unsigned int AE_WARMUP_FRAMES = 10;
+	unsigned int AE_WARMUP_FRAMES = 20;
 	unsigned int AE_WARMUP_CADENCE_SECONDS = 20;
 	auto ae_last_warmup_time = std::chrono::high_resolution_clock::now();
 	float time_since_last_ae_warmup = 0.0f;
@@ -314,10 +320,12 @@ static void event_loop(LibcameraRaw &app, GpioHandler* lampHandler)
 		if (autoExposureEnabled) {
 			if (ae_warmup_in_progress) {
 				ae_warmup_frames_captured++;
+				if (AE_WARMUP_FRAMES - ae_warmup_frames_captured == 3) {
+					lockAutoExposure(app);
+				}
 				if (ae_warmup_frames_captured >= AE_WARMUP_FRAMES) {
 					ae_warmup_in_progress = false;
 					ae_warmup_frames_captured = 0;
-					lockAutoExposure(app);
 					if (lampHandler) {
 						lampHandler->setNextLampColor();
 					}
